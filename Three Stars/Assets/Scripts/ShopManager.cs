1a8b8c26b27c4d61f89797acd7bdc9d4b5a07ba0
Ethan Akiyama <ekakiyam@ucsc.edu>
2026-02-23 21:46:58 -0800

    More Shop Work
    Worked on Shop Manager

--- Three Stars/Assets/Scripts/ShopManager.cs
+++ Three Stars/Assets/Scripts/ShopManager.cs
@@ -1,20 +1,50 @@
-using System.Collections;
+using ThreeStars;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject upgadeCardPrefab;
+    public Transform shopContentParent;
+    public List<UpgradeCard> availableUpgrades;
+
+    private void Awake()
     {
-        
+        availableUpgrades = new List<UpgradeCard>(Resources.LoadAll<UpgradeCard>("Upgrade Card Data"));
+    }
+    public void OpenShop()
+    {
+        ClearShop();
+        GenerateCards(3);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClearShop()
     {
-        
+        foreach (Transform child in shopContentParent)
+        {
+            Destroy(child.gameObject);
+        }
     }
 
-    public void OpenShop() { }
+    private void GenerateCards(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            UpgradeCard randomUpgrade = availableUpgrades[Random.Range(0, availableUpgrades.Count)];
+            GameObject cardGO = Instantiate(upgadeCardPrefab, shopContentParent);
+            UpgradeCardDisplay cardDisplay = cardGO.GetComponent<UpgradeCardDisplay>();
+            cardDisplay.cardData = randomUpgrade;
+            cardDisplay.UpdateCardDisplay();
+
+            Button btn = cardGO.GetComponent<Button>();
+            btn.onClick.AddListener(() => PurchaseUpgrade(randomUpgrade, cardGO));
+        }
+    }
+
+    public void PurchaseUpgrade(UpgradeCard upgrade, GameObject visual)
+    {
+        ProgressionManager.Instance.ApplyUpgrade(upgrade);
+        Destroy(visual);
+    }
 }
